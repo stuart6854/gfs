3733c4a22f47af3c36bce17efd887fd4c3f31f0f
Stuart Millman <stu.millman15@gmail.com>
2023-11-20 20:58:49 +0000

    Add Filesystem implementation. Add Filesystem doc comments.

--- include/gfs/filesystem.hpp
+++ include/gfs/filesystem.hpp
@@ -9,6 +9,9 @@
 namespace gfs
 {
     using MountID = uint32_t;
+    using FileID = uint64_t;
+
+    constexpr MountID InvalidMountId = 0;
 
     class FileImporter;
 
@@ -29,11 +32,32 @@ namespace gfs
             bool AllowUnmount;
         };
 
+        /**
+         * @brief
+         * @param rootDir
+         * @param allowUnmount
+         * @return 0 if failed to mount directory. >0 if successful.
+        */
         auto MountDir(const std::filesystem::path& rootDir, bool allowUnmount = true) -> MountID;
+
+        /**
+         * @brief
+         * @param id
+         * @return True if a was unmounted.
+        */
         bool UnmountDir(MountID id);
 
+        /**
+         * @brief
+         * @param rootDir
+         * @return 0 if mount does not exist. Otherwise, returns the mounts id.
+        */
         auto GetMountId(const std::filesystem::path& rootDir) -> MountID;
 
+        /**
+         * @brief Runs the given function for each mounted directory.
+         * @param func
+        */
         void ForEachMount(const std::function<void(const Mount&)>& func);
 
         //////////////////////////////////////////////////////////////////////////
@@ -42,27 +66,62 @@ namespace gfs
 
         struct File
         {
-            uint64_t Uuid;
-            MountID MountId;
-            std::filesystem::path MountRelPath;
+            FileID Uuid; // The Unique Identifier of the file.
+            MountID MountId; // The Id of the mount this file is in.
+            std::filesystem::path MountRelPath;// Path to this file relative to the mount it is in.
+            std::vector<FileID> FileDependencies; // Files this file references.
         };
 
-        auto GetFile(uint64_t id) -> const File*;
+        /**
+         * @brief
+         * @param id
+         * @return
+        */
+        auto GetFile(FileID id) -> const File*;
 
+        /**
+         * @brief
+         * @param func
+        */
         void ForEachFile(const std::function<void(const File& file)>& func);
 
         //////////////////////////////////////////////////////////////////////////
         // Import
         //////////////////////////////////////////////////////////////////////////
 
+        /**
+         * @brief
+         * @param fileExts
+         * @param importer
+        */
         void SetImporter(const std::vector < std::string>& fileExts, const std::shared_ptr<FileImporter>& importer);
+
+        /**
+         * @brief
+         * @param fileExt
+         * @return
+        */
         auto GetImporter(const std::string& fileExt) -> std::shared_ptr<FileImporter>;
 
         //////////////////////////////////////////////////////////////////////////
         // Utility
         //////////////////////////////////////////////////////////////////////////
 
+        /**
+         * @brief
+         * @param path
+         * @param mountId
+         * @return
+         * @attention Unsure if this works as intended. Taken from https://stackoverflow.com/questions/61123627/check-if-an-stdfilesystempath-is-inside-a-directory
+        */
         bool IsPathInMount(const std::filesystem::path& path, MountID mountId);
+
+        /**
+         * @brief
+         * @param path
+         * @return
+         * @attention Unsure if this works as intended. See `IsPathInMount()`.
+        */
         bool IsPathInAnyMount(const std::filesystem::path& path);
 
     private:
@@ -71,7 +130,6 @@ namespace gfs
     private:
         std::vector<Mount> m_mounts;
         std::unordered_map<MountID, Mount*> m_mountIdMap;
-        std::unordered_map<size_t, Mount*> m_mountPathMap;
         MountID m_nextMountId = 1;
 
         std::vector<File> m_files;
--- src/gfs/filesystem.cpp
+++ src/gfs/filesystem.cpp
@@ -1,6 +1,121 @@
 #include "gfs/filesystem.hpp"
 
+#include <cassert>
+
 namespace gfs
 {
+    auto Filesystem::MountDir(const std::filesystem::path& rootDir, bool allowUnmount) -> MountID
+    {
+        if (!std::filesystem::exists(rootDir))
+            return InvalidMountId;
+
+        if (!std::filesystem::is_directory(rootDir))
+            return InvalidMountId;
+
+        auto& mount = m_mounts.emplace_back();
+        mount.RootDirPath = rootDir;
+        mount.AllowUnmount = allowUnmount;
+        mount.Id = m_nextMountId++;
+
+        m_mountIdMap[mount.Id] = &mount;
+
+        assert(mount.Id != InvalidMountId);
+        return mount.Id;
+    }
+
+    bool Filesystem::UnmountDir(MountID id)
+    {
+        auto* mount = GetMount(id);
+        if (!mount)
+            return false;
+
+        return mount->Id;
+    }
+
+    auto Filesystem::GetMountId(const std::filesystem::path& rootDir) -> MountID
+    {
+        const auto rootDirAbs = std::filesystem::absolute(rootDir);
+        for (const auto& mount : m_mounts)
+        {
+            const auto mountDirAbs = std::filesystem::absolute(mount.RootDirPath);
+            if (rootDirAbs == mountDirAbs)
+                return mount.Id;
+        }
+        return InvalidMountId;
+    }
+
+    void Filesystem::ForEachMount(const std::function<void(const Mount&)>& func)
+    {
+        for (const auto& mount : m_mounts)
+            func(mount);
+    }
+
+    auto Filesystem::GetFile(FileID id) -> const File*
+    {
+        const auto it = m_fileIdMap.find(id);
+        if (it == m_fileIdMap.end())
+            return nullptr;
+
+        return it->second;
+    }
+
+    void Filesystem::ForEachFile(const std::function<void(const File& file)>& func)
+    {
+        for (const auto& file : m_files)
+            func(file);
+    }
+
+    void Filesystem::SetImporter(const std::vector < std::string>& fileExts, const std::shared_ptr<FileImporter>& importer)
+    {
+        for (const auto& ext : fileExts)
+        {
+            const auto extHash = std::hash<std::string>{}(ext);
+            m_extImporterMap[extHash] = importer;
+        }
+    }
+
+    auto Filesystem::GetImporter(const std::string& fileExt) -> std::shared_ptr<FileImporter>
+    {
+        const auto extHash = std::hash<std::string>{}(fileExt);
+        const auto it = m_extImporterMap.find(extHash);
+        if (it == m_extImporterMap.end())
+            return nullptr;
+
+        return it->second;
+    }
+
+    bool Filesystem::IsPathInMount(const std::filesystem::path& path, MountID mountId)
+    {
+        auto* mount = GetMount(mountId);
+        if (!mount)
+            return false;
+
+        const auto finalPath = std::filesystem::canonical(mount->RootDirPath / path);
+
+        auto [rootEnd, nothing] = std::mismatch(mount->RootDirPath.begin(), mount->RootDirPath.end(), finalPath.begin());
+        if (rootEnd != mount->RootDirPath.end())
+            return false;
+
+        return true;
+    }
+
+    bool Filesystem::IsPathInAnyMount(const std::filesystem::path& path)
+    {
+        for (const auto& mount : m_mounts)
+        {
+            if (IsPathInMount(path, mount.Id))
+                return true;
+        }
+        return false;
+    }
+
+    auto Filesystem::GetMount(MountID id) -> Mount*
+    {
+        const auto it = m_mountIdMap.find(id);
+        if (it == m_mountIdMap.end())
+            return nullptr;
+
+        return it->second;
+    }
 
 }
